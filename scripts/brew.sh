a7ccef2e31ec4e533e176a64b7da0d871f56f506
Mirko May <mail@mirkomay.com>
2017-12-11 01:27:10 +1300

    brew cleanup

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,40 +1,35 @@
 #!/bin/sh
 
-if ! is-macos -o ! is-executable ruby -o ! is-executable curl -o ! is-executable git; then
-  echo "Skipped: Homebrew (missing: ruby, curl and/or git)"
-  return
-fi
-
 echo "Installing brew packages..."
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap 'homebrew/versions'
 brew tap 'homebrew/dupes'
 brew tap 'homebrew/php'
-brew tap 'Goles/battery'
 
 brew update
 brew upgrade
 
+# Core
 brew install antigen
 brew install coreutils
-brew install composer
 brew install dockutil
-brew install git
+brew install htop
+brew install rbenv
+brew install nvm
+brew install python
+brew install zsh
+
+# Extra
+brew install composer
 brew install homebrew/apache/httpd24 --with-http2 --with-privileged-ports
 brew install homebrew/php/php70 --with-httpd24
-brew install htop
 brew install kubernetes-cli
 brew install mysql
 brew install multitail
-brew install nvm
-brew install python
-brew install rbenv
 brew install ssh-copy-id
 brew install wget --with-iri
 brew install watchman
 brew install yarn
-brew install zsh
 
 brew services restart mysql
 
