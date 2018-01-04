57a6769b2a9ab09ab691735fbedb4806168e03c3
Mirko May <mail@mirkomay.com>
2018-01-05 10:08:20 +1300

    Updated cron

--- cron/every-hour.sh
+++ cron/every-hour.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 rsync -avz --exclude='.DS_Store' --filter="dir-merge,- .gitignore" ~/Work/* ~/Dropbox/Work
-# unison -auto -batch -ignore='.DS_Store' --filter="dir-merge,- .gitignore" ~/Work/* ~/Dropbox/Work
