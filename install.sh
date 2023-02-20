1926cafae3f13bfac99a49d2c012f4c9c2890d08
Mirko May <mayko780@gmail.com>
2023-02-21 00:19:18 +1300

    Update

--- install.sh
+++ install.sh
@@ -1,55 +1,44 @@
 #!/bin/sh
 
-# PROMPT_TIMEOUT=15
-
 ################################################################
 # Install new user for MacOS
 ################################################################
-install_apps () {
 
-  # # Make zsh your default shell
-  # chsh -s /bin/zsh
+# Make zsh your default shell
+chsh -s /bin/zsh
 
-  # Ask for the administrator password upfront
-  sudo -v
+# Ask for the administrator password upfront
+sudo -v
 
-  # Keep-alive: update existing `sudo` time stamp until finished
-  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# Keep-alive: update existing `sudo` time stamp until finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-  # Update software
-  sudo softwareupdate -i -a
+# Update software
+sudo softwareupdate -i -a
 
-  # Install Command Line Tools for Xcode (Available on a fresh install of macOS)
-  sudo xcode-select --install
+# Install Command Line Tools for Xcode (Available on a fresh install of macOS)
+sudo xcode-select --install
 
-  # Install Homebrew
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  brew update
-  brew upgrade
-  brew bundle --global --file ./dotfiles/Brewfile
+brew update
+brew upgrade
+brew bundle --global --file ./Brewfile
 
-  # Setup Mac System defaults
-  sh .macos
+# Setup Mac System defaults
+sh .macos
 
-  # Setup Mac Dock
-  ./scripts/macos_dock.sh
+# Setup Mac Dock
+./scripts/macos_dock.sh
 
-  # Symlinks dotfiles into ~/
-  ./scripts/symlink-dotfiles.sh
+# Symlinks dotfiles into ~/
+./scripts/symlink-dotfiles.sh
 
-  # Symlinks dropbox dotfiles into ~/
-  # ./scripts/symlink-dotfiles-dropbox.sh
+# Symlinks dropbox dotfiles into ~/
+# ./scripts/symlink-dotfiles-dropbox.sh
 
-  echo "Installation complete."
+echo "Installation complete."
 
-  echo "Just one more thing."
-  echo "In order to run cronjobs you need to run `crontab` manually (symlinks do NOT work). Please see 'crontab' file."
-}
-
-# Confirm user is happy to proceed with package installations
-# echo -e "\nDid you create a backup? (y/N)"
-# read -t $PROMPT_TIMEOUT -n 1 -r
-# if [[ $REPLY =~ ^[Yy]$ ]]; then
-  install_apps
-# fi
+echo "Just one more thing."
+echo "In order to run cronjobs you need to run `crontab` manually (symlinks do NOT work). Please see 'crontab' file."
